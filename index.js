f060a2bdb8171fe233bd0d2ad306582588cb0882
Alfredo Paz <apaz0@ucol.mx>
2021-04-29 20:08:35 -0500

    Edit: uso de mysql

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let mysql = require('mysql');
+
+let conn = mysql.createConnection({
+    host: 'localhost',
+    database: 'chat_progra',
+    user: 'root',
+    password: ''    
+});
+
+conn.connect(function(err){
+    if(err){
+        throw err 
+    }else{
+        console.log('Conexion exitosa!');
+    }
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/chat.html'); 
@@ -12,9 +28,18 @@ io.on('connection', (socket) => {
     let username; 
     socket.on('crearUsuario', function(data){
         username = data;
+
+        conn.query('INSERT INTO usuarios(nombre_usuario, fecha) VALUES ("'+ username +'", CURDATE())')
+
     });
 
     socket.on('mjsNuevo', function(data){
+
+        socket.broadcast.emit('mensaje',{
+            usuario: username,
+            mensaje: data
+        });
+
         socket.emit('mensaje', {
             usuario: username,
             mensaje: data
